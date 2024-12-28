711e297398a0e1a02fb8c4365bc86de2e3f1a211
ebut <ebut@kakao.com>
2024-12-28 11:54:12 +0900

    이력서를 등록/수정/삭제 메소드 추가 및 수정

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,6 +2,8 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { MinioModule } from 'nestjs-minio-client';
+import * as https from 'https';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
